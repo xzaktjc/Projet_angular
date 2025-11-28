639bbd00ae87c8c6a492aa525ecd1fcac4246b33
🚀 <clapie.florent@gmail.com>
2025-11-28 11:48:35 +0100

    feat: enhance FirstName component with input handling and display updates

--- src/app/features/first-name/first-name.ts
+++ src/app/features/first-name/first-name.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-first-name',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './first-name.html',
   styleUrl: './first-name.scss',
 })
 export class FirstName {
   firstName = 'Angular';
 
+  newFirstName = '';
+
   firstNameAvailables = ['Angular', 'React', 'Vue', 'Svelte'];
   public changeFirstName(newName: string): void {
     this.firstName = newName;
+  }
+
+  public addFirstName(firstName: string): void {
+    if (!firstName) {
+      return;
+    }
+
+    const isUnique = !this.firstNameAvailables.includes(firstName);
+    if (isUnique) {
+      this.firstNameAvailables.push(firstName);
+      this.newFirstName = '';
+    }
   }
 }
