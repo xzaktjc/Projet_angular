e58ae72af2ee609165cfa6c4814c89a39246ec60
🚀 <clapie.florent@gmail.com>
2025-12-11 11:40:48 +0100

    feat: update List component to display planet names and enhance API response handling

--- src/app/starwars/list/list.ts
+++ src/app/starwars/list/list.ts
@@ -1,8 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { map } from 'rxjs';
 
-interface Planet {}
+interface Result<T> {
+  count: number;
+  next: string;
+  previous: string | null;
+  results: T[];
+}
+
+interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-list',
@@ -14,8 +37,6 @@ export class List {
   private http = inject(HttpClient);
 
   planets$ = this.http
-    .get<Planet[]>('https://swapi.dev/api/planets/')
-    .pipe
-    // map((response) => response.results),
-    ();
+    .get<Result<Planet>>('https://swapi.dev/api/planets/')
+    .pipe(map((res) => res.results));
 }
